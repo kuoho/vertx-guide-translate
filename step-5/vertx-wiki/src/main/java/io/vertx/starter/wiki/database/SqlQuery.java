c24e2cd48f797cf1b7ac93b73ec9eae7f670658d
guohao <guoho1991@gmail.com>
2018-10-13 14:10:40 +0800

    init step-5

---
+++ step-5/vertx-wiki/src/main/java/io/vertx/starter/wiki/database/SqlQuery.java
@@ -0,0 +1,14 @@
+package io.vertx.starter.wiki.database;
+
+/**
+ * @author guohao
+ * @date 2018-10-10 14:09
+ */
+enum SqlQuery {
+    CREATE_PAGES_TABLE,
+    ALL_PAGES,
+    GET_PAGE,
+    CREATE_PAGE,
+    SAVE_PAGE,
+    DELETE_PAGE
+}
---
+++ step-5/vertx-wiki/src/main/java/io/vertx/starter/wiki/database/WikiDatabaseService.java
@@ -0,0 +1,43 @@
+package io.vertx.starter.wiki.database;
+
+import io.vertx.codegen.annotations.Fluent;
+import io.vertx.codegen.annotations.ProxyGen;
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Handler;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.jdbc.JDBCClient;
+
+import java.util.HashMap;
+
+/**
+ * @author guohao
+ * @date 2018-10-10 14:04
+ */
+@ProxyGen
+public interface WikiDatabaseService {
+
+    @Fluent
+    WikiDatabaseService fetchAllPages(Handler<AsyncResult<JsonArray>> handler);
+
+    @Fluent
+    WikiDatabaseService fetchPage(String page, Handler<AsyncResult<JsonObject>> resultHandler);
+
+    @Fluent
+    WikiDatabaseService createPage(String title, String markdown, Handler<AsyncResult<Void>> resultHandler);
+
+    @Fluent
+    WikiDatabaseService savePage(int id, String markdown, Handler<AsyncResult<Void>> resultHandler);
+
+    @Fluent
+    WikiDatabaseService deletePage(int id, Handler<AsyncResult<Void>> resultHandler);
+
+    static WikiDatabaseService create(JDBCClient dbClient, HashMap<SqlQuery, String> sqlQueries, Handler<AsyncResult<WikiDatabaseService>> readyHandler) {
+        return new WikiDatabaseServiceimpl(dbClient, sqlQueries, readyHandler);
+    }
+
+    static WikiDatabaseService createProxy(Vertx vertx, String address) {
+        return new WikiDatabaseServiceVertxEBProxy(vertx, address);
+    }
+}
